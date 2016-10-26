01b83499d9b2b27ffb636d8666b925878d022daf
Victor Toschi <victor.toschi@hotmail.com>
2016-10-26 19:03:28 -0200

    First test files

---
+++ Jasmine/spec/major-minor-spec.js
@@ -0,0 +1,8 @@
+describe('Major and Minor', () => {
+
+    it ('Should understand unordered numbers', () => {
+        let exec = new majorMinor()
+        exec.encontra([5,45,23,8,1])
+    })
+
+})
